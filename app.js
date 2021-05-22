10a8976f2f79fe4c934e6198d408ad83096a5837
jimshana <jimshana2193@gmail.com>
2021-05-22 13:45:08 +0530

    login page completed

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
 var expressSanitizer = require("express-sanitizer");
 var methodOverride = require("method-override");
 var app = express();
+
+const userRoute = require("./routes/user.route");
+const User = require("./models/user.model");
+
 
 // App Configs
 mongoose.connect("mongodb://localhost/Bukz_app");
@@ -12,6 +18,28 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
+
+
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 999999999,
+        rolling: true
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 var bookSchema = mongoose.Schema({
     number:String,
@@ -24,14 +52,41 @@ var bookSchema = mongoose.Schema({
     link:String,
     year:String,
 	genre: String,
-	rating: String
+	rating: String,
+	
+	
+	
+	
 });
 
 var Book = mongoose.model("Book",bookSchema);
+var viewSchema = mongoose.Schema({
+    name:String,
+	rating: String,
+	commenter:String,
+	
+	
+	
+});
+
+var View = mongoose.model("View",viewSchema);
+
+
+
+
+
+app.use("/", userRoute);
+
 
 app.get("/",function(req,res){
 	res.redirect("/books");
 });
+
+
+
+
+
+
 
 app.get("/books",function(req,res){
 	Book.find({},function(err,books){
@@ -43,9 +98,24 @@ app.get("/books",function(req,res){
 	});
 });
 
+
+app.get("/about",function(req,res){
+	View.find({},function(err,about){
+		if(err){
+			console.log("Error!");
+		} else {
+			res.render("comment",{about:about});
+		}
+	});
+});
+
+
 app.get("/books/new",function(req,res){
 	res.render("new");
 });
+
+
+
 
 app.post("/books",function(req,res){
 	req.body.book.title = req.sanitize(req.body.book.title);
@@ -80,6 +150,20 @@ app.get("/books/:id/edit",function(req,res){
 	});
 });
 
+
+app.post("/about",function(req,res){
+	req.body.view.name = req.sanitize(req.body.view.name);
+	req.body.view.commenter = req.sanitize(req.body.view.commenter);
+	
+	View.create(req.body.view,function(err,newBook){
+		if(err){
+			res.render("edit");
+		} else {
+			res.redirect("/about");
+		}
+	});
+});
+
 app.put("/books/:id",function(req,res){
 	req.body.book.title = req.sanitize(req.body.book.title);
 	req.body.book.description = req.sanitize(req.body.book.description);
@@ -93,6 +177,8 @@ app.put("/books/:id",function(req,res){
 	});
 });
 
+
+
 app.delete("/books/:id",function(req,res){
 	Book.findByIdAndRemove(req.params.id,function(err){
 		if(err){
@@ -103,6 +189,15 @@ app.delete("/books/:id",function(req,res){
 	});
 });
 
+app.delete("/about/:id",function(req,res){
+	View.findByIdAndRemove(req.params.id,function(err){
+		if(err){
+			res.redirect("/about");
+		} else {
+			res.redirect("/about");
+		}
+	});
+});
 var port = process.env.PORT || 3000;
 app.listen(port,process.env.IP,function(){
 	console.log("The Book Review App server has started")
